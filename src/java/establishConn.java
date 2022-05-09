921c7634383ea5c6e42adbcd6b2740dc5d4ac97d
AACGiron <84278532+AACGiron@users.noreply.github.com>
2022-05-09 18:26:53 +0800

    started -ero

---
+++ src/java/establishConn.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author acer
+ */
+public class establishConn extends HttpServlet {
+
+    Connection conn;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        try {
+            Class.forName(config.getInitParameter("jdbcClassName"));
+            //System.out.println("jdbcClassName: " + config.getInitParameter("jdbcClassName"));
+            String username = config.getInitParameter("dbUserName");
+            String password = config.getInitParameter("dbPassword");
+            StringBuffer url = new StringBuffer(config.getInitParameter("jdbcDriverURL"))
+                    .append("://")
+                    .append(config.getInitParameter("dbHostName"))
+                    .append(":")
+                    .append(config.getInitParameter("dbPort"))
+                    .append("/")
+                    .append(config.getInitParameter("databaseName"));
+            conn = DriverManager.getConnection(url.toString(), username, password);
+        } catch (SQLException sqle) {
+            System.out.println("SQLException error occured - "
+                    + sqle.getMessage());
+        } catch (ClassNotFoundException nfe) {
+            System.out.println("ClassNotFoundException error occured - "
+                    + nfe.getMessage());
+        }
+    } 
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("conn", conn);
+        if(conn!=null){
+        request.getRequestDispatcher("LoginPage.jsp").forward(request, response);
+        }
+        else{
+            request.getRequestDispatcher("Error.jsp").forward(request, response);
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
